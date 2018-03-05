64bb71e6d4afaebf69042259d5977e96ecead7e1
rpdict <rpdict@gmail.com>
2018-03-05 13:47:05 +0800

    Delete the Useless code

--- initial/src/main/java/hello/MainController.java
+++ initial/src/main/java/hello/MainController.java
@@ -1,17 +1,12 @@
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 @Controller    // This means that this class is a Controller
 @RequestMapping(path = "/demo") // This means URL's start with /demo (after Application path)
